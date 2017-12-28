22c26af55de2515ca5f863825856d490d2301dfc
doublexinori <598439233@qq.com>
2017-12-28 14:53:30 +0800

    Update jenkins.py

--- jenkins.py
+++ jenkins.py
@@ -1,18 +1,21 @@
 from selenium import webdriver
 from time import sleep
 
+username = input('username:')
+password = input('password:')
+
 try:
     pts = webdriver.ChromeOptions()
     pts.binary_location = ("D:/Program Files/Google/Chrome/Application/chrome.exe")
     driver = webdriver.Chrome(chrome_options=pts, executable_path="D:/selenium_test/chromedriver.exe")
-    driver.get('http://192.168.1.222:9020/user/toLogin.action')
+    driver.get(url)
     driver.maximize_window()
     driver.implicitly_wait(30)
-    driver.find_element_by_id('loginStoreId').send_keys('18601407983')
+    driver.find_element_by_id('loginStoreId').send_keys(username)
     sleep(3)
     driver.find_element_by_xpath("//input[@id='loginUserPwd1']").click()
     sleep(3)
-    driver.find_element_by_xpath("//input[@id='loginUserPwd']").send_keys('123456')
+    driver.find_element_by_xpath("//input[@id='loginUserPwd']").send_keys(password)
     sleep(3)
     driver.find_element_by_class_name('qd_btn').click()
     driver.implicitly_wait(10)
